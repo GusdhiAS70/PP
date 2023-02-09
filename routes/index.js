9f3cee88de9f08c1bf1908764fe83d4fa6a73112
GusdhiAS70 <azizsetiadhi@gmail.com>
2023-02-09 15:09:11 +0700

    benerin ejs course

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const Controller = require("../controllers/controller")
 const router = express.Router()
 
-// router.get('/', Controller.home)
+router.get('/', Controller.renderLogin)
 router.get('/course', Controller.course)
 router.get('/course/form', Controller.renderFormRegister)
 router.post('/course/form', Controller.handlerFormRegister)
